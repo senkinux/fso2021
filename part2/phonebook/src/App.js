d714f964feea00837f573c26fc082ddc7d5cb6dc
als92 <senkinas@gmail.com>
2021-03-24 17:34:34 +0000

    2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,11 +3,13 @@ import React, { useState } from "react"
 const App = () => {
 	const [persons, setPersons] = useState([{ name: "Arto Hellas" }])
 	const [newName, setNewName] = useState("")
+	const [newNumber, setNewNumber] = useState("")
 
 	const handleSubmit = e => {
 		e.preventDefault()
 		const nameObj = {
 			name: newName,
+			number: newNumber,
 		}
 
 		if (persons.find(({ name }) => name === newName)) {
@@ -15,6 +17,7 @@ const App = () => {
 		} else {
 			setPersons(persons.concat(nameObj))
 			setNewName("")
+			setNewNumber("")
 			return
 		}
 	}
@@ -28,13 +31,22 @@ const App = () => {
 					<input value={newName} onChange={e => setNewName(e.target.value)} />
 				</div>
 				<div>
+					number:{" "}
+					<input
+						value={newNumber}
+						onChange={e => setNewNumber(e.target.value)}
+					/>
+				</div>
+				<div>
 					<button type="submit">add</button>
 				</div>
 			</form>
 			<h2>Numbers</h2>
 			<div>
 				{persons.map(person => (
-					<p key={person.name}>{person.name}</p>
+					<p key={person.name}>
+						{person.name} {person.number}
+					</p>
 				))}
 			</div>
 		</div>
