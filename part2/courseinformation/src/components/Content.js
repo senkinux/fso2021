50b2f822c82643dfb25bca0692fc0c7abe375574
als92 <senkinas@gmail.com>
2021-03-24 15:56:43 +0000

    2.2

--- part2/courseinformation/src/components/Content.js
+++ part2/courseinformation/src/components/Content.js
@@ -2,11 +2,20 @@ import React from "react"
 import Part from "./Part"
 
 const Content = ({ parts }) => {
+	const total = parts.reduce((prev, cur) => prev + cur.exercises, 0)
+
+	console.log(total)
+
 	return (
 		<div>
-			{parts.map(part => (
-				<Part key={part.id} name={part.name} exercises={part.exercises} />
-			))}
+			<div>
+				{parts.map(part => (
+					<Part key={part.id} name={part.name} exercises={part.exercises} />
+				))}
+			</div>
+			<div>
+				<strong>total of {total} exercises</strong>
+			</div>
 		</div>
 	)
 }
