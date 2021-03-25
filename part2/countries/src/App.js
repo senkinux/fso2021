acc8749c6b1161920c9a493b5b829079ea88aaea
als92 <senkinas@gmail.com>
2021-03-25 10:54:54 +0000

    2.12

---
+++ part2/countries/src/App.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+
+const App = () => {
+	const [countries, setCountries] = useState([])
+	const [keyword, setKeyword] = useState("")
+
+	useEffect(() => {
+		axios
+			.get("https://restcountries.eu/rest/v2/all")
+			.then(res => setCountries(res.data))
+	}, [])
+
+	const handleChange = e => {
+		setKeyword(e.target.value)
+	}
+
+	const filteredCountries = countries.filter(person =>
+		person.name.toLowerCase().startsWith(keyword.toLowerCase())
+	)
+
+	return (
+		<div>
+			<div>
+				find countries: <input value={keyword} onChange={handleChange} />
+			</div>
+			<div>
+				{filteredCountries.length > 1 && filteredCountries.length < 10
+					? filteredCountries.map(country => (
+							<p key={country.name}>{country.name}</p>
+					  ))
+					: filteredCountries.length > 10
+					? "Too many matches, specify another filter"
+					: filteredCountries.map(country => (
+							<div key={country.name}>
+								<h2>{country.name}</h2>
+								<p>capital {country.capital}</p>
+								<p>population {country.population}</p>
+								<h2>languages</h2>
+								<ul>
+									{country.languages.map(lang => (
+										<li key={lang.name}>{lang.name}</li>
+									))}
+								</ul>
+								<br />
+								<img
+									src={country.flag}
+									style={{ maxHeight: 100, maxWidth: 100 }}
+									alt="flag"
+								></img>
+							</div>
+					  ))}
+			</div>
+		</div>
+	)
+}
+
+export default App
