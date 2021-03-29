71ce455022eb49ff9b9e0609d6aeb0e6831740a6
als92 <senkinas@gmail.com>
2021-03-29 15:28:00 +0100

    3.10

--- part2/phonebook/src/services/services.js
+++ part2/phonebook/src/services/services.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const baseUrl = "http://localhost:3001/persons"
+const baseUrl = "https://vast-earth-70136.herokuapp.com/api/persons"
 
 const getAll = () => {
 	const req = axios.get(baseUrl)
