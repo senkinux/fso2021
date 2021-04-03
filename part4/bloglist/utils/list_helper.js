b1c97c830bdbfe90e424907704f957322190c38d
als92 <senkinas@gmail.com>
2021-04-03 09:16:45 +0100

    4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,4 +3,14 @@ const dummy = blogs => 1
 const totalLikes = blogposts => {
 	return blogposts.reduce((acc, cur) => acc + cur.likes, 0)
 }
-module.exports = { dummy, totalLikes }
+
+const favoriteBlog = blogs => {
+	const favoriteBlog = blogs.reduce((prev, cur) =>
+		prev.likes < cur.likes ? cur : prev
+	)
+	delete favoriteBlog.__v
+	delete favoriteBlog._id
+	return favoriteBlog
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
