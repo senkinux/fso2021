39ba700e14b2cd7543f0117a60a67be0cb718e22
als92 <senkinas@gmail.com>
2021-04-07 16:15:20 +0100

    4.18

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose")
 const middleware = require("./utils/middleware")
 const blogsRouter = require("./controllers/blogs")
 const usersRouter = require("./controllers/users")
+const loginRouter = require("./controllers/login")
 
 mongoose
 	.connect(config.MONGODB_URI, {
@@ -21,6 +22,7 @@ app.use(cors())
 app.use(express.json())
 app.use("/api/blogs", blogsRouter)
 app.use("/api/users", usersRouter)
+app.use("/api/login", loginRouter)
 app.use(middleware.errorHandler)
 
 module.exports = app
