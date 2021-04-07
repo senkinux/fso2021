962ee48f304b042ffe90f686fbcf0c24f8685e6e
als92 <senkinas@gmail.com>
2021-04-07 16:50:10 +0100

    4.19

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,27 +1,42 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
 const User = require("../models/user")
+const jwt = require("jsonwebtoken")
 
-blogsRouter.get("/", async (request, response) => {
+const getToken = req => {
+	const auth = req.get("authorization")
+	if (auth && auth.toLowerCase().startsWith("bearer")) {
+		return auth.substring(7)
+	}
+	return null
+}
+
+blogsRouter.get("/", async (req, res) => {
 	const blogs = await Blog.find({}).populate("user", {
 		username: 1,
 		name: 1,
 		id: 1,
 	})
-	response.json(blogs)
+	res.json(blogs)
 })
 
-blogsRouter.post("/", async (request, response) => {
-	const { body } = request
+blogsRouter.post("/", async (req, res) => {
+	const { body } = req
 	if (!body.likes) {
 		body.likes = 0
 	}
 
 	if (!body.url || !body.title) {
-		return response.status(400).end()
+		return res.status(400).end()
 	}
 
-	const user = await User.findById(body.userId)
+	const token = getToken(req)
+	const decodedToken = jwt.verify(token, process.env.SECRET)
+	if (!token || !decodedToken.id) {
+		return res.status(401).json({ error: "token is invalid or missing" })
+	}
+
+	const user = await User.findById(decodedToken.id)
 
 	const blog = new Blog({
 		title: body.title,
@@ -34,20 +49,20 @@ blogsRouter.post("/", async (request, response) => {
 	const savedBlog = await blog.save()
 	user.blogs = user.blogs.concat(savedBlog)
 	await user.save()
-	response.status(201).json(savedBlog)
+	res.status(201).json(savedBlog)
 })
 
-blogsRouter.delete("/:id", async (request, response) => {
-	await Blog.findByIdAndDelete(request.params.id)
-	response.status(204).end()
+blogsRouter.delete("/:id", async (req, res) => {
+	await Blog.findByIdAndDelete(req.params.id)
+	res.status(204).end()
 })
 
-blogsRouter.put("/:id", async (request, response) => {
-	const body = request.body
-	const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, body, {
+blogsRouter.put("/:id", async (req, res) => {
+	const body = req.body
+	const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, body, {
 		new: true,
 	})
-	response.json(updatedBlog)
+	res.json(updatedBlog)
 })
 
 module.exports = blogsRouter
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,4 +1,5 @@
 const errorHandler = (err, req, res, next) => {
+	console.log(err)
 	if ((err.name = "ValidationError")) {
 		return res.status(400).send({
 			error: "username and password are required and username must be unique",
