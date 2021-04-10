a810668b0b0916f319617ae211a0b3d20f93de9d
als92 <senkinas@gmail.com>
2021-04-10 14:13:58 +0100

    5.8

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,29 @@
-import React from "react"
-const Blog = ({ blog }) => (
-	<div>
-		{blog.title} {blog.author}
-	</div>
-)
+import React, { useState } from "react"
+const Blog = ({ blog }) => {
+	const [blogVisible, setBlogVisible] = useState(false)
 
+	const blogStyle = {
+		paddingTop: 10,
+		paddingLeft: 2,
+		border: "solid",
+		borderWidth: 1,
+		marginBottom: 5,
+	}
+
+	return (
+		<div style={blogStyle}>
+			{blog.title} {blog.author}
+			<button onClick={() => setBlogVisible(!blogVisible)}>view</button>
+			{!blogVisible ? null : (
+				<div>
+					<div>{blog.url}</div>
+					<div>
+						likes {blog.likes} <button>like</button>
+					</div>
+					<div>{blog.author}</div>
+				</div>
+			)}
+		</div>
+	)
+}
 export default Blog
