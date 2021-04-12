761e2f7c1e3d322ef907ad6ccb322b5ea228018e
als92 <senkinas@gmail.com>
2021-04-12 11:12:29 +0100

    5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "@testing-library/jest-dom/extend-expect"
-import { render } from "@testing-library/react"
+import { render, fireEvent } from "@testing-library/react"
 import Blog from "./Blog"
 
 describe("<Blog />", () => {
@@ -10,11 +10,22 @@ describe("<Blog />", () => {
 		url: "dummyurl.com",
 		likes: 0,
 	}
+
 	test("displays author and title but not url or likes", () => {
 		const component = render(<Blog blog={dummyBlog} />)
 		expect(component.container).toHaveTextContent(dummyBlog.author)
 		expect(component.container).toHaveTextContent(dummyBlog.title)
 		expect(component.container).not.toHaveTextContent(dummyBlog.url)
 		expect(component.container).not.toHaveTextContent(dummyBlog.likes)
+	})
+
+	test("url and likes are displayed after click", () => {
+		const component = render(
+			<Blog blog={dummyBlog} list={[]} setBlogs={() => {}} />
+		)
+		const btn = component.getByText("view")
+		fireEvent.click(btn)
+		expect(component.container).toHaveTextContent(dummyBlog.url)
+		expect(component.container).toHaveTextContent(dummyBlog.likes)
 	})
 })
