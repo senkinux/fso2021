274e16c4ce3b3008c7dd9567a452b05740a2af77
als92 <senkinas@gmail.com>
2021-04-14 14:21:51 +0100

    fixing indentation problem of vs code and github

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -2,35 +2,35 @@ import React from "react"
 import { useSelector, useDispatch } from "react-redux"
 
 const App = () => {
-	const anecdotes = useSelector(state => state)
-	const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
 
-	const vote = id => {
-		// console.log("vote", id)
-		dispatch({ type: "VOTE", payload: { id } })
-	}
+  const vote = id => {
+    // console.log("vote", id)
+    dispatch({ type: "VOTE", payload: { id } })
+  }
 
-	return (
-		<div>
-			<h2>Anecdotes</h2>
-			{anecdotes.map(anecdote => (
-				<div key={anecdote.id}>
-					<div>{anecdote.content}</div>
-					<div>
-						has {anecdote.votes}
-						<button onClick={() => vote(anecdote.id)}>vote</button>
-					</div>
-				</div>
-			))}
-			<h2>create new</h2>
-			<form>
-				<div>
-					<input />
-				</div>
-				<button>create</button>
-			</form>
-		</div>
-	)
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+      <h2>create new</h2>
+      <form>
+        <div>
+          <input />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  )
 }
 
 export default App
