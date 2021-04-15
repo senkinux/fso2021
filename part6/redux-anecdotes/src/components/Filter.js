c47add6b4b3a2c89847d0280936741dd1f168e36
als92 <senkinas@gmail.com>
2021-04-15 18:04:38 +0100

    removed unnecessary comments

--- part6/redux-anecdotes/src/components/Filter.js
+++ part6/redux-anecdotes/src/components/Filter.js
@@ -1,15 +1,11 @@
 import React from "react"
 import { setFilter } from "../reducers/filterReducer"
-import { useSelector, useDispatch } from "react-redux"
+import { useDispatch } from "react-redux"
 
 const Filter = () => {
   const dispatch = useDispatch()
-  // const anecdotes = useSelector(state => {
-
-  // })
 
   const handleChange = event => {
-    // input-field value is in variable event.target.value
     const { value } = event.target
     dispatch(setFilter(value))
   }
