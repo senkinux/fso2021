b67159869b0b3966358cadec17a457b63a4de4fe
als92 <senkinas@gmail.com>
2021-04-15 15:53:32 +0100

    6.11

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { likeBlog } from "../reducers/anecdoteReducer"
+import { setMessage, removeMessage } from "../reducers/notificationReducer"
 
 const AnecdoteList = () => {
   const anecdotes = useSelector(state => state.anecdotes)
@@ -8,6 +9,14 @@ const AnecdoteList = () => {
 
   const vote = id => {
     dispatch(likeBlog(id))
+    const filteredAnecdote = anecdotes.filter(a =>
+      a.id === id ? a.content : ""
+    )
+    const likedAnecdote = filteredAnecdote[0].content
+    dispatch(setMessage(likedAnecdote))
+    setTimeout(() => {
+      dispatch(removeMessage())
+    }, 5000)
   }
 
   return (
--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,9 +1,24 @@
-const initialState = "Dummy text"
+const initialState = ""
+
+export const setMessage = message => {
+  return {
+    type: "SET_MESSAGE",
+    payload: message,
+  }
+}
+
+export const removeMessage = () => {
+  return {
+    type: "REMOVE_MESSAGE",
+  }
+}
 
 const notificationReducer = (state = initialState, action) => {
   switch (action.type) {
     case "SET_MESSAGE":
       return action.payload
+    case "REMOVE_MESSAGE":
+      return initialState
     default:
       return state
   }
