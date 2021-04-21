4adf03a9151dd675b193e76fdcfba9bebfd2fa47
als92 <senkinas@gmail.com>
2021-04-21 09:27:18 +0100

    7.1

---
+++ part7/routed-anecdotes/src/App.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react"
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom"
+import Menu from "./components/Menu"
+import About from "./components/About"
+import Footer from "./components/Footer"
+import AnecdoteList from "./components/AnecdoteList"
+import CreateNew from "./components/CreateNew"
+
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: "If it hurts, do it more often",
+      author: "Jez Humble",
+      info: "https://martinfowler.com/bliki/FrequencyReducesDifficulty.html",
+      votes: 0,
+      id: "1",
+    },
+    {
+      content: "Premature optimization is the root of all evil",
+      author: "Donald Knuth",
+      info: "http://wiki.c2.com/?PrematureOptimization",
+      votes: 0,
+      id: "2",
+    },
+  ])
+
+  const [notification, setNotification] = useState("")
+
+  const addNew = anecdote => {
+    anecdote.id = (Math.random() * 10000).toFixed(0)
+    setAnecdotes(anecdotes.concat(anecdote))
+  }
+
+  const anecdoteById = id => anecdotes.find(a => a.id === id)
+
+  const vote = id => {
+    const anecdote = anecdoteById(id)
+
+    const voted = {
+      ...anecdote,
+      votes: anecdote.votes + 1,
+    }
+
+    setAnecdotes(anecdotes.map(a => (a.id === id ? voted : a)))
+  }
+
+  return (
+    <div>
+      <h1>Software anecdotes</h1>
+      <Menu />
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/create">
+          <CreateNew addNew={addNew} />
+        </Route>
+        <Route path="/">
+          <AnecdoteList anecdotes={anecdotes} />
+        </Route>
+      </Switch>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
