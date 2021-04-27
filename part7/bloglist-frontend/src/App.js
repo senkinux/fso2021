5f263b19f34d997800748c8149a4150d1d6b809c
als92 <senkinas@gmail.com>
2021-04-27 09:53:18 +0100

    7.11

--- part7/bloglist-frontend/src/App.js
+++ part7/bloglist-frontend/src/App.js
@@ -5,10 +5,14 @@ import Message from "./components/Message"
 import ShowBlogForm from "./components/ShowBlogForm"
 import blogService from "./services/blogs"
 import { useSelector, useDispatch } from "react-redux"
-import { initializeBlogs, addNewBlog } from "./reducers/blogsReducer"
+import {
+  initializeBlogs,
+  addNewBlog,
+  likeBlog,
+  deleteBlog,
+} from "./reducers/blogsReducer"
 
 const App = () => {
-  const [hookBlogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
   const [blogFormVisible, setBlogFormVisible] = useState(false)
 
@@ -34,39 +38,16 @@ const App = () => {
     setUser(null)
   }
 
-  const likeHandler = async blog => {
-    const likedBlog = {
-      author: blog.author,
-      likes: blog.likes + 1,
-      title: blog.title,
-      url: blog.url,
-      user: blog.user.id,
-    }
-
-    const response = await blogService.update(blog.id, likedBlog)
-    const updatedBlogIdx = blogs.findIndex(b => b.id === response.id)
-    const listCopy = [...blogs]
-    listCopy[updatedBlogIdx] = response
-    setBlogs(listCopy)
+  const likeHandler = blog => {
+    dispatch(likeBlog(blog))
   }
 
   const createBlog = blog => {
     dispatch(addNewBlog(blog))
   }
 
-  const deleteHandler = async blog => {
-    const confirmation = window.confirm(
-      `Are you sure you want to delete ${blog.title} by ${blog.author}`
-    )
-    if (confirmation) {
-      try {
-        await blogService.deleteBlog(blog.id)
-        const fetchedBlogs = await blogService.getAll()
-        setBlogs(fetchedBlogs)
-      } catch (error) {
-        window.alert("Ooops, something went wrong")
-      }
-    }
+  const deleteHandler = blog => {
+    dispatch(deleteBlog(blog))
   }
 
   return (
--- part7/bloglist-frontend/src/reducers/blogsReducer.js
+++ part7/bloglist-frontend/src/reducers/blogsReducer.js
@@ -11,8 +11,41 @@ const blogsReducer = (state = [], action) => {
       return action.payload
     case "CREATE_BLOG":
       return [...state, action.payload]
+    case "LIKE_BLOG":
+      return state.map(blog =>
+        blog.id !== action.payload.id ? blog : action.payload
+      )
+    case "DELETE_BLOG":
+      return [...action.payload]
     default:
       return state
+  }
+}
+
+export const likeBlog = blog => async dispatch => {
+  const likedBlog = {
+    author: blog.author,
+    likes: blog.likes + 1,
+    title: blog.title,
+    url: blog.url,
+    user: blog.user.id,
+  }
+  const response = await blogService.update(blog.id, likedBlog)
+  dispatch({ type: "LIKE_BLOG", payload: response })
+}
+
+export const deleteBlog = blog => async dispatch => {
+  const confirmation = window.confirm(
+    `Are you sure you want to delete ${blog.title} by ${blog.author}`
+  )
+  if (confirmation) {
+    try {
+      await blogService.deleteBlog(blog.id)
+      const fetchedBlogs = await blogService.getAll()
+      dispatch({ type: "DELETE_BLOG", payload: fetchedBlogs })
+    } catch (error) {
+      window.alert("Ooops, something went wrong")
+    }
   }
 }
 
