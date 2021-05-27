08c7bbd9e532b24107928cdcc3c6dccb84e12b93
als92 <senkinas@gmail.com>
2021-05-27 18:01:52 +0100

    add right apollo pkg

--- part8/library-frontend/src/index.js
+++ part8/library-frontend/src/index.js
@@ -1,5 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client"
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "http://localhost:4000",
+  }),
+})
+
+const query = gql`
+  query {
+    allBooks {
+      title
+      author
+      published
+      genres
+    }
+  }
+`
+
+client.query({ query }).then(res => console.log(res.data))
+
+ReactDOM.render(<App />, document.getElementById("root"))
