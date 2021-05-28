f6f86e6005389ad03cb4c094ea631b9ddeba1885
als92 <senkinas@gmail.com>
2021-05-28 10:59:28 +0100

    8.11

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,8 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
+import { useMutation } from "@apollo/client"
+import { ALL_AUTHORS, EDIT_AUTHOR } from "../queries"
 
 const Authors = ({ show, authors }) => {
+  const [name, setName] = useState("")
+  const [born, setBorn] = useState("")
+  const [updateAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  })
+
   if (!show) {
     return null
+  }
+
+  const submitHandler = e => {
+    e.preventDefault()
+
+    updateAuthor({ variables: { name, setBornTo: Number(born) } })
+
+    setName("")
+    setBorn("")
   }
 
   return (
@@ -24,6 +41,24 @@ const Authors = ({ show, authors }) => {
           ))}
         </tbody>
       </table>
+      <h2>Set birth year</h2>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="name">name: </label>
+        <input
+          name="name"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+        <br />
+        <label htmlFor="born">born :</label>
+        <input
+          name="born"
+          value={born}
+          onChange={e => setBorn(e.target.value)}
+        />
+        <br />
+        <button>update author</button>
+      </form>
     </div>
   )
 }
